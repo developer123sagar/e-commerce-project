5ffffc3aebede36dc016ba0578684fd99b26195e
Anusha <anushashrestha7890@gmail.com>
2023-06-13 19:07:54 +0545

    about us page

--- src/App.js
+++ src/App.js
@@ -9,13 +9,7 @@ import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <About/>
       </>
   );
 }
