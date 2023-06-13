cd663d7ea9b8a7c58379ed594c0c5968b581d628
Isha <ishadheke4@gmail.com>
2023-06-13 18:19:58 +0545

    home page file added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -24,7 +24,7 @@ const Login = (props) => {
        <input className='border p-2  mr-2 w-full' type="email" placeholder='Youremail@gmail.com' autocomplete="off" name="email" id="email" />
         <input className='border p-2 mt-5 mr-2 w-full' type="password" placeholder='Password' name="password" id="password" />
        </div>
-       <button className='w-full py-2 my-2 bg-primary-700 mix-blend-multiply hover:bg-green-700 text-white'>Sign In</button>
+       <button className='w-full py-2 my-2 bg-primary-700 mix-blend-multiply hover:bg-green-700 text-white mt-6'>Sign In</button>
        <p className='text-center'>Forget password or username?</p>
       
       </form>
