6590aef8c35747bafae953c67372809a09720203
Sagar chand <chandsagar314@gmail.com>
2023-06-19 15:20:22 +0545

    changes in main branch

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import home from 'images/home.jpg'
 const Main = () => {
   return (
     <>
-      <div className="w-full h-screen bg-gray-200 mix-blend-multiply -m-1 p-0">
+      <div className="w-full h-screen bg-gray-200 mix-blend-multiply -mt-[6px] font-roboto">
         <div className="h-full w-2/3 bg-primary-700 mix-blend-multiply">
           <div className="absolute top-20 left-10 w-70 md: flex-row items-center sm:text-sm">
             <h1 className="text-5xl font-bold text-center mb-4 mt-20 text-secondary-400 ">
